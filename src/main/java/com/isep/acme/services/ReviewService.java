ff5232d25d5347d62dd77655b30ba39c547ab910
Tiago Lacerda <1220285@isep.ipp.pt>
2023-04-16 19:20:56 +0100

    added transactional

--- src/main/java/com/isep/acme/services/ReviewService.java
+++ src/main/java/com/isep/acme/services/ReviewService.java
@@ -1,9 +1,12 @@
 package com.isep.acme.services;
 
+import com.isep.acme.model.Vote;
 import com.isep.acme.model.dtos.CreateReviewDTO;
 import com.isep.acme.model.Review;
 import com.isep.acme.model.dtos.ReviewDTO;
+import com.isep.acme.model.dtos.VoteReviewDTO;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 public interface ReviewService {
@@ -15,4 +18,7 @@ public interface ReviewService {
     List<ReviewDTO> findPendingReview();
 
     List<ReviewDTO> findReviewsByUser(Long userID);
+
+    @Transactional
+    boolean addVoteToReview(VoteReviewDTO voteReviewDTO, Vote vote);
 }
--- src/main/java/com/isep/acme/services/impl/ReviewServiceImpl.java
+++ src/main/java/com/isep/acme/services/impl/ReviewServiceImpl.java
@@ -3,7 +3,9 @@ package com.isep.acme.services.impl;
 import com.isep.acme.model.Product;
 import com.isep.acme.model.Review;
 import com.isep.acme.model.User;
+import com.isep.acme.model.Vote;
 import com.isep.acme.model.dtos.ReviewDTO;
+import com.isep.acme.model.dtos.VoteReviewDTO;
 import com.isep.acme.model.mappers.ReviewMapper;
 import com.isep.acme.repositories.ProductRepository;
 import com.isep.acme.repositories.ReviewRepository;
@@ -12,6 +14,7 @@ import com.isep.acme.services.ReviewService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
@@ -65,5 +68,23 @@ public class ReviewServiceImpl implements ReviewService {
         }
 
         return null;
+    }
+
+    @Transactional
+    @Override
+    public boolean addVoteToReview(VoteReviewDTO voteReviewDTO, Vote vote) {
+
+        Optional<Review> review = this.repository.findByRID(voteReviewDTO.getRID());
+
+        if (review.isEmpty()) return false;
+
+        if (voteReviewDTO.getVote().equalsIgnoreCase("upVote") && review.get().addUpVote(vote)
+                || voteReviewDTO.getVote().equalsIgnoreCase("downVote") && review.get().addDownVote(vote)) {
+
+            repository.save(review.get());
+            return true;
+
+        }
+        return false;
     }
 }
